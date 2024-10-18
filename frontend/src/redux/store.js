59e67a33f9b7c68e33d58571a61a440087c5d623
Nikita <romanovnnikita@gmail.com>
2024-10-19 02:40:35 +0300

    Добавлен redux slice для добавления, удаления книги

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import booksReducer from "./books/reducer";
+// import booksReducer from "./books/reducer";
+import bookSlice from "./slices/bookSlice";
 import filterReducer from "./slices/filterSlice";
 
 const store = configureStore({
-  reducer: { books: booksReducer, filter: filterReducer },
+  reducer: { books: bookSlice, filter: filterReducer },
 });
 
 export default store;
